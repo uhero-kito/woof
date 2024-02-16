b9fb4e575d60c8530bf8b11dd87daac3095677c1
KITO, takayuki <takayuki.kito@uhero.co.jp>
2024-02-16 16:15:21 +0900

    docs: update PHPDoc for LogFormat, DefaultLogFormat

--- src/Log/DefaultLogFormat.php
+++ src/Log/DefaultLogFormat.php
@@ -2,18 +2,22 @@
 
 namespace Woof\Log;
 
+/**
+ * 標準的な形式でアプリケーションログを書式化する LogFormat の実装です。
+ */
 class DefaultLogFormat implements LogFormat
 {
     /**
-     * date() の引数として使用されるフォーマットです
+     * date() の引数として使用されるフォーマットです。
      *
      * @var string
      */
     private $dateFormat;
 
     /**
+     * 指定された日付フォーマットでオブジェクトを生成します。
      *
-     * @param string $dateFormat date() 関数で使用可能なフォーマット文字列
+     * @param string $dateFormat date() 関数で使用可能なフォーマット文字列。省略時は "Y-m-d H:i:s" が適用されます
      */
     public function __construct(string $dateFormat = "")
     {
@@ -21,11 +25,12 @@ class DefaultLogFormat implements LogFormat
     }
 
     /**
+     * 指定された情報を指定の書式でフォーマットした文字列を返します。
      *
-     * @param string $message
-     * @param int $time
-     * @param int $level
-     * @return string
+     * @param string $message 出力するログメッセージ
+     * @param int $time ログの発生時刻 (Unix time)
+     * @param int $level ログレベル
+     * @return string フォーマットされたログ文字列
      */
     public function format(string $message, int $time, int $level): string
     {
@@ -37,8 +42,8 @@ class DefaultLogFormat implements LogFormat
     /**
      * 指定された定数に応じたラベル ("ERROR", "INFO" など) を返します。
      *
-     * @param int $level
-     * @return string
+     * @param int $level ログレベル定数
+     * @return string 対応するログレベルのラベル文字列
      */
     private function formatLogLevel(int $level): string
     {
--- src/Log/LogFormat.php
+++ src/Log/LogFormat.php
@@ -10,10 +10,10 @@ interface LogFormat
     /**
      * 指定されたメッセージ・発生時刻・ログレベルによるログ出力を書式化します。
      *
-     * @param string $message
-     * @param int $time
-     * @param int $level
-     * @return string
+     * @param string $message 出力するログメッセージ
+     * @param int $time ログの発生時刻 (Unix time)
+     * @param int $level ログレベル
+     * @return string フォーマットされたログ文字列
      */
     public function format(string $message, int $time, int $level): string;
 }
