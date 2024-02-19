3e4e8523295502ef39a0c099a18a0421144df214
KITO, takayuki <takayuki.kito@uhero.co.jp>
2024-02-19 15:41:55 +0900

    docs: update PHPDoc for StringDecoder, IniDecoder, JsonDecoder

--- src/Util/JsonDecoder.php
+++ src/Util/JsonDecoder.php
@@ -2,19 +2,29 @@
 
 namespace Woof\Util;
 
+/**
+ * JSON 形式の文字列を配列に変換する StringDecoder の実装です。
+ *
+ * このクラスは直接インスタンス化することはできません。
+ * getInstance() メソッドを使用してインスタンスを取得してください。
+ */
 class JsonDecoder implements StringDecoder
 {
     /**
+     * 外部からのインスタンス生成を禁止することで getInstance() の使用を強制します。
+     *
      * @codeCoverageIgnore
      */
     private function __construct()
     {
-
     }
 
     /**
-     * @param string $src
-     * @return array
+     * JSON 形式の文字列を連想配列に変換して返します。
+     * パースに失敗した場合や、結果が配列でなかった場合は空の配列を返します。
+     *
+     * @param string $src パース対象の JSON 形式の文字列
+     * @return array 変換された配列 (失敗時は空の配列)
      */
     public function parse(string $src): array
     {
@@ -23,7 +33,9 @@ class JsonDecoder implements StringDecoder
     }
 
     /**
-     * @return JsonDecoder
+     * このクラスの唯一のインスタンスを取得します。
+     *
+     * @return JsonDecoder JsonDecoder インスタンス
      */
     public static function getInstance(): self
     {
--- src/Util/StringDecoder.php
+++ src/Util/StringDecoder.php
@@ -2,7 +2,20 @@
 
 namespace Woof\Util;
 
+/**
+ * 文字列を配列にパース (変換) するためのインタフェースです。
+ *
+ * INI や JSON といった特定のデータフォーマットの文字列を配列に変換する際に使用します。
+ * また、このインタフェースを独自に実装することで、任意のフォーマットでシリアライズされたデータを配列に変換する、
+ * 独自のデコーダを作成することも可能です。
+ */
 interface StringDecoder
 {
+    /**
+     * 指定された文字列を配列に変換します。
+     *
+     * @param string $src パース対象の文字列
+     * @return array 変換された配列
+     */
     public function parse(string $src): array;
 }
